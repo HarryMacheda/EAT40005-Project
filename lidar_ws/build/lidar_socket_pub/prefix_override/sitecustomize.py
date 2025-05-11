b9d96467ebf3378724f514966596095824b0170e
angelinath21 <angelina.benhardi@gmail.com>
2025-05-11 15:24:06 +1000

    added ros2 rviz visualisation on lidar data

---
+++ lidar_ws/build/lidar_socket_pub/prefix_override/sitecustomize.py
@@ -0,0 +1,4 @@
+import sys
+if sys.prefix == '/usr':
+    sys.real_prefix = sys.prefix
+    sys.prefix = sys.exec_prefix = '/home/angelina/EAT40005-Project/lidar_ws/install/lidar_socket_pub'
---
+++ lidar_ws/build/openslam_gmapping/gridfastslam/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/angelina/EAT40005-Project/lidar_ws/src/slam_gmapping/openslam_gmapping/gridfastslam
+# Build directory: /home/angelina/EAT40005-Project/lidar_ws/build/openslam_gmapping/gridfastslam
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
